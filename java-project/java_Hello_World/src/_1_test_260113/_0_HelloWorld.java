009dee962fab45d2b1098d590e2b45ef31acb555
JOLDO <bbat115@gmail.com>
2026-01-13 10:53:32 +0900

    first commit

---
+++ java-project/java_Hello_World/src/_1_test_260113/_0_HelloWorld.java
@@ -0,0 +1,8 @@
+package _1_test_260113;
+
+public class _0_HelloWorld {
+    public static void main(String[] args) {    //main이라고 적어서 함수 만들어주기 기본 처음으로 들어가는 함수
+        System.out.println("Hello World");
+        System.out.println();   //sout라고 치면 이게 적힘
+    }
+}
